7c7133f5945415a54ada5ab23054b08aff15e837
Uchenna <ansonuchenna@gmail.com>
2023-06-06 08:46:50 +0100

    Amazon best sellers

---
+++ src/books.js
@@ -0,0 +1,22 @@
+export const books = [
+  {
+    author: 'Piggyback',
+    title: 'The Legend of Zelda™',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91vwHMt+x7L._AC_UL600_SR600,400_.jpg',
+    id: 1,
+  },
+
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg',
+    id: 2,
+  },
+
+  {
+    author: 'Robert Greene',
+    title: 'The 48 Laws of Power',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71aG+xDKSYL._AC_UL600_SR600,400_.jpg',
+    id: 3,
+  },
+];
